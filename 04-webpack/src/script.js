86ccac9f12887b02d63271113a87e01233097ddf
JayTailor45 <tailorj64@gmail.com>
2021-12-24 15:32:45 +0530

    Transformations finished

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -21,8 +21,38 @@ scene.add(mesh);
 
 // Add camera to we can see the scene
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+
+// Camara position
 camera.position.z = 3;
+
 scene.add(camera);
+
+// Mesh position
+// mesh.position.x = 3;
+// mesh.position.y = 3;
+// mesh.position.z = 3;
+
+console.log(mesh.position.length());
+console.log(mesh.position.distanceTo(camera.position));
+// mesh.position.normalize();
+mesh.position.set(0.7, -0.6, 1);
+
+// Axes Helper - Display helper grid
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+// Scale 
+// mesh.scale.x = 2;
+// mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+// mesh.rotation.y = 2;
+// mesh.rotation.reorder('yxz');
+// mesh.rotation.x = 3;
+// mesh.rotation.y = 2;
+
+// Quaternion
+camera.lookAt(mesh.position);
 
 // Renderer - To render screen through the camera point of view
 const canvas = document.querySelector(".webgl");
